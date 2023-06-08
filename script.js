b258f993f747d530597f0cded99865ccd68f9613
aribaHabib <123486335+aribaHabib@users.noreply.github.com>
2023-06-08 17:05:06 +0500

    add animation

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+  let starter = document.querySelector(".loader");
+  starter.classList.add("hide");
+}, 1800);
+
+// let header = document.querySelector(".navbar");
+// window.addEventListener("scroll", function () {
+//   header.classList.toggle("scroll", window.scrollY > 0);
+// });
