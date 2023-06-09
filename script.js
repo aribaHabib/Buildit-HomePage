6f728fcb6889b7083dda4aec57f71d136c837bd1
aribaHabib <123486335+aribaHabib@users.noreply.github.com>
2023-06-09 16:09:48 +0500

    add slideshow

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ setTimeout(() => {
   starter.classList.add("hide");
 }, 1800);
 
-// let header = document.querySelector(".navbar");
-// window.addEventListener("scroll", function () {
-//   header.classList.toggle("scroll", window.scrollY > 0);
-// });
+let header = document.querySelector(".navbar");
+window.addEventListener("scroll", function () {
+  header.classList.toggle("scroll", window.scrollY > 0);
+});
+
+var content = document.getElementById("scrollContent");
+
+function scrollPrev() {
+  content.appendChild(content.firstElementChild);
+}
+
+function scrollNext() {
+  content.insertBefore(content.lastElementChild, content.firstElementChild);
+}
